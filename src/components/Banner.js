fc930e92337e7c219ada556a556c24feaeea7eae
Trex1888 <harpua123@gmail.com>
2021-05-31 23:14:58 -0500

    3rd commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,10 @@
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Banner() {
   return (
     <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
       <Carousel
         autoPlay
         infiniteLoop
@@ -12,17 +14,19 @@ function Banner() {
         interval={5000}
       >
         <div>
-          <img loading="lazy" src="" alt="" />
+          <img loading="lazy" src="https://links.papareact.com/gi1" alt="img" />
         </div>
+
         <div>
-          <img loading="lazy" src="" alt="" />
+          <img loading="lazy" src="https://links.papareact.com/6ff" alt="img" />
         </div>
+
         <div>
-          <img loading="lazy" src="" alt="" />
+          <img loading="lazy" src="https://links.papareact.com/7ma" alt="img" />
         </div>
       </Carousel>
     </div>
   );
 }
-//120
+
 export default Banner;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,56 @@
-function Product() {
+import Image from "next/image";
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
+
+const MIN_RATING = 1;
+const MAX_RATING = 5;
+
+function Product({ id, title, price, description, category, image }) {
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+  const [hasPrime] = useState(Math.random() < 0.5);
+
   return (
-    <div>
-      <h2>product</h2>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p
+        className="absolute top-2 right-2 text-xs italic text-gray-400
+      text-transform: uppercase"
+      >
+        {category}
+      </p>
+
+      <Image src={image} height={200} width={200} objectFit="contain" />
+
+      <h4 className="my-3">{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500" />
+          ))}
+      </div>
+
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+      <div className="mb-5">
+        <Currency quantity={price} currency="USD" />
+      </div>
+
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img
+            className="w-12"
+            src="https://links.papareact.com/fdw"
+            alt="prime"
+          />
+          <p className="text-xs text-gray-500">FREE Next-Day Delivery!</p>
+        </div>
+      )}
+
+      <button className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
